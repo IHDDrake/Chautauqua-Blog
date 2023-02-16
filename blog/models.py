b9fcdc9fd7796994391f3a51caf11edf9e088092
Ian Drake <ihddrake@gmail.com>
2023-02-16 16:33:57 -0500

    Comment Update 2

--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,26 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.urls import reverse
 
+#Post Model
 class Post(models.Model):
     heroHeader = models.ImageField(upload_to ='uploads/', default=None, blank=True)
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    #Auto Increments on every visit of the Detailed Post page.
     viewCount = models.IntegerField(default=0)
+
+    #This is a boolean variable visible in the admin menu that can be toggled on or off depending on whether
+    #the admin would like that post to be publically viewable.
     pubStatus = models.BooleanField(default=False)
 
+    #Basic Object to string method
     def __str__(self):
-        return self.title + ' | ' + str(self.author)
+        return self.title + ' by ' + str(self.author)
 
+    #Returns Url for the Details page of a sepcific post
     def get_absolute_url(self):
         return reverse('detailed_post', args=[str(self.id)])
 
@@ -22,12 +29,14 @@ class Comment(models.Model):
     name = models.CharField(max_length=255)
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    #These variables are kept seperately so that the admins may know the total amount of upvotes and downvotes a specific comment has.
     upVote = models.IntegerField(default=0)
     downVote = models.IntegerField(default=0)
 
-
+    #Basic Object to string method
     def __str__(self):
-        return self.post.title + ' - ' + str(self.name)
+        return self.post.title + ' by ' + str(self.name)
 
 
 
--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from .views import Home, Detail, addPost, upVote, downVote
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('', Home.as_view(), name="homepage"),
     path('posts/<int:pk>', Detail.as_view(), name="detailed_post"),
+    #This url goes unused in this program
     path('add_post/', addPost.as_view(), name ='add_post'),
+
+    #The upvote and downvote are passed the primary key of the post so
+    #they know where to return to after completing their tasks.
     path('upVote/<int:pk>', upVote, name = 'upvote'),
     path('downVote/<int:pk>', downVote, name = 'downvote')
 
 ]
-urlpatterns += staticfiles_urlpatterns()
 
+#adding Static Files
+urlpatterns += staticfiles_urlpatterns()
